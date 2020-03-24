8256dbd583f6d6a38c70f6ab365b07a5bab8a5b9
himanshu rathore <himanshuclash@gmail.com>
2020-03-25 02:28:40 +0530

    Prompt of who wins added

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ var Randomnumber2 = Math.floor(Math.random()*6) + 1
 
 document.querySelector('.img1').setAttribute('src','images/dice'+Randomnumber + '.png')
 document.querySelector('.img2').setAttribute('src','images/dice'+Randomnumber2 + '.png')
+if (Randomnumber > Randomnumber2) {
+  document.querySelector('h1').textContent = 'Player one wins ! 🚩'
+
+} else if (Randomnumber < Randomnumber2){
+
+  document.querySelector('h1').textContent = 'Player two wins ! 🚩'
+} else{
+  document.querySelector('h1').textContent = 'Tie ! 🚩'
+}
